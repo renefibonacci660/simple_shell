f35eb5bd4b9b1063d996da06be6573078292f90b
renefibonacci660 <660@holbertonschool.com>
2019-04-16 20:48:01 +0000

    Gets path, compares, concats andcounts strings/lengths and passes styling checker successfully

--- getPath.c
+++ getPath.c
@@ -1,46 +1,63 @@
 #include "holberton.h"
-
-void getPath(char **ex)
+/**
+ * getPath - Gets the tokenized input and attempts to find
+ * the executable from PATH and executes it if found
+ * @ex: The tokenized inptu
+ * Return: If the input's excutable path was not found, return NULL
+ */
+char *getPath(char **ex)
 {
-	char *tok, *thePath;
+	char *tok = NULL;
+	char *thePath = NULL;
 	char buf[100];
-	char *check = NULL;
 	size_t i;
+	char *check = NULL;
 	struct stat st;
 
-	for(i = 0; environ[i] != NULL; i++)
-	{
-		tok = strtok(environ[i], "=");
-		
-		if(strcmp(tok, "PATH") == 0)
-		{
-			thePath = strtok(NULL, environ[i]);
-			break;
-		}
-	}
-	tok = strtok(thePath, ":");
-
+	if (ex == NULL)
+		return (NULL);
+	thePath = getVar("PATH"); /*Recives the directories of PATH*/
+	if (thePath == NULL) /*If variable was non existent*/
+		thePath = "";
+	if (checkCurrent(thePath) == 1)
+		if (stat(ex[0], &st) == 0)
+			return (NULL);
+	tok = strtok(thePath, ":"); /*Checks for all possiblie directories*/
 	while (tok != NULL)
 	{
-		tok = strtok(NULL, ":");
-		strcpy(buf, tok);
-		check = filePath(buf, ex[0]);
-
-		if(stat(check, &st) == 0)
+		_strcpy(buf, tok);        /*Makes a copy of*/
+		tok = strtok(NULL, ":");  /*to allow leave token alone*/
+		if (tok == NULL)
 			break;
+		check = filePath(buf, ex[0]);
+		if (check != NULL && stat(check, &st) == 0)
+		{
+			if (execve(check, ex, NULL) == -1)
+				return (NULL); /*If excutable failed*/
+		}
+		for (i = 0; i < 100; i++)
+			buf[i] = 0;
 	}
-	if (execve(check, ex, NULL) == -1)
-		perror("Error"); /** Check how error messages print */
+	return (NULL);
 }
 
+/**
+ *filePath - Concatenates path and command to
+ * check if it's an executable
+ *@path: The path to check for executable
+ *@command: The input command to attach to path
+ *Return: If the concantenated path and command are
+ *an executable, return it. Otherwise it will be NULL
+ */
 char *filePath(char *path, char *command)
 {
 	struct stat st;
-	strcat(path, "/");
-	strcat(path, command);
 
-	if(stat(path, &st) == 0)
+	if (path == NULL || command == NULL)
+		return (NULL);
+	_strcat(path, "/");
+	_strcat(path, command);
+	if (stat(path, &st) == 0)
 		return (path);
-
 	return (NULL);
 }
