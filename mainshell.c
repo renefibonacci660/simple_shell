308809434436fe4267874e52a4eeb424232e3b57
renefibonacci660 <660@holbertonschool.com>
2019-04-17 17:13:36 +0000

    Not completed but adding file to allow for debug checking of other files

--- mainshell.c
+++ mainshell.c
@@ -1,50 +1,12 @@
 #include "holberton.h"
 
-int main()
+/**
+ *main - runs the shell program
+ *Return: returns 0 if successfull
+ */
+int main(void)
 {
-	size_t size;
-	int len;
-	pid_t process;
-	char *input;
-	char **args;
-	struct stat st;
-
-	while(1)
-	{
-		if (isatty(0))
-			printf("$ ");
-
-		len = getline(&input, &size, stdin);
-
-		if(len == EOF)
-			break;
-
-		input[len - 1] = '\0'; /** test whether needed because token later */
-		args = token(input);
-		process = fork();
-		
-		if (process == -1)
-		{
-			perror("Error:\n");
-			return (1);
-		}
-
-		if (process == 0)
-		{
-			if (stat(args[0], &st) == 0)
-			{
-				if (execve(args[0], args, NULL) == -1)
-					perror("Error: ");
-			}
-			else
-			{
-					getPath(args);
-			}
-		}
-		else
-		{
-			wait(NULL);
-			free(args);
-		}
-	}
+	signal(SIGINT, signal_handler);
+	_shell();
+	return (0);
 }
