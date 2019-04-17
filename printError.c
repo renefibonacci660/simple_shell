e9ea1c0927bf69107acdcc5396dd543dc70bdd07
renefibonacci660 <660@holbertonschool.com>
2019-04-17 17:15:37 +0000

    Now printing correct error messages and passing style checker

---
+++ printError.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *printError - prints errors in shell format
+ *@str: The command that will be printed in the error
+*/
+void printError(char *str)
+{
+	size_t len;
+
+	len = strLen(str); /*errno is a number that is assigned my erros*/
+	if (errno == ENOTDIR || errno == ENOENT)
+	{
+		write(2, str, len);
+		write(2, ": Command not found\n", 20);
+	}
+}
